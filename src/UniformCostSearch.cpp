a9ea356de07d43ee1a9a226886cebfdc89c3f433
Donovan Pilcher <donovanep03@gmail.com>
2024-05-01 13:42:25 -0700

    implemented a base version of the uniform cost search. Will need changes to deal with unsolvable puzzles. We also need to determine what exactly these searches return.

--- src/UniformCostSearch.cpp
+++ src/UniformCostSearch.cpp
@@ -1,7 +1,34 @@
 //Currently being worked on by Donovan and Adwin
 #include "UniformCostSearch.h"
+#include <queue>
+
+using namespace std;
 
 void UniformCostSearch(Puzzle board)
 {
-    
+    queue<Node*> moves;
+    Tree* movesTree;
+    Node* currNode = nullptr;
+    Node* start = new Node(board, 0, 0);
+    moves.push(start);
+    movesTree->SetHead(start);
+    while(!moves.empty())
+    {
+        currNode = moves.front();
+        if(currNode->GetPuzzle().isGoal())
+        {
+            return;
+        }
+        vector<Puzzle> succ;
+        succ = currNode->GetPuzzle().successors();
+        for(unsigned int i = 0; i < succ.size(); i++)
+        {
+            Node* next = new Node(succ[i],currNode->cost + 1, 0);
+            currNode->SetChild(next);
+            next->SetParent(currNode);
+            moves.push(next);
+        }
+        moves.pop();
+    }
+    return;
 }
